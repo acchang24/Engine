e4107da210dcd04d09f2b768c537e37a7366fb15
acchang24 <acchang24@gmail.com>
2022-05-02 01:57:12 -0700

    Message loop and WndProc

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -1,5 +1,19 @@
 #include <Windows.h>
 
+// Define a custom windows procedure
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance, 
 	HINSTANCE hPrevInstance, 
@@ -17,7 +31,7 @@ int CALLBACK WinMain(
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -31,6 +45,42 @@ int CALLBACK WinMain(
 	RegisterClassEx(&wc);
 
 
-	// TODO: Create an instance of the window
+	// Create an instance of the window
+	HWND hWnd = CreateWindowEx(
+		0,
+		pClassName,
+		L"The Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	// Show the window
+	ShowWindow(hWnd, SW_SHOW);
+
+	// Message pump
+	MSG msg;
+
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
+
+
 	return 0;
 }
